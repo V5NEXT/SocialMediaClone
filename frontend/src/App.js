63cf0fc523265eaef0ad7a738d4e68cf8ebed625
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-11-22 20:58:56 +0100

    added custom configs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
-  );
+    Hello world!
+  </h1>  )
 }
 
-export default App;
+export default App
