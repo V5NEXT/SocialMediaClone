a209b482ca32652422956a1cd602f73e91678dfe
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-11-24 00:29:07 +0100

    login page UI complete

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -7,6 +7,10 @@ import logo from '../assets/logowhite.png';
 
 
 const Login = () => {
+const responseGoogle = () =>{
+  
+}
+
   return (
     <div className='flex justify-start items-center flex-col h-screen'>
       <div className='relative w-full h-full'>
@@ -22,6 +26,24 @@ const Login = () => {
           <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
                 <div className='p-5'>
                   <img src={logo} width="130px" alt="logo"/>
+                </div>
+                <div className='shadow-2xl'>
+                  <GoogleLogin
+                   clientId=''
+                   render={(renderProps)=>(
+                      <button
+                       type='button'
+                       className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+                       onClick={renderProps.onClick}
+                       disabled={renderProps.disabled}
+                       >
+                        <FcGoogle className='mr-4'/>Google Sign Up
+                      </button>
+                   )}
+                   onSuccess={responseGoogle}
+                   onFailure={responseGoogle}
+                   cookiePolicy="single_host_origin"
+                   />
                 </div>
           </div>
       </div>
