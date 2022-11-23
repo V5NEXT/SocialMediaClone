68d89d2ea3460e3cb318100e341d081b45e908fc
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-11-23 23:03:26 +0100

    Added Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>  )
+    <Routes>
+      <Route path='login' element={<Login/>}/>
+      <Route path='/*' element={<Home/>}/>
+    </Routes>
+    )
 }
 
 export default App
