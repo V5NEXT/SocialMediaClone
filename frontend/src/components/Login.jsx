4ad739d477b2e45a417b95233e406eaf81b4fbae
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-11-24 00:20:17 +0100

    login page intermediate commit

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -12,8 +12,18 @@ const Login = () => {
       <div className='relative w-full h-full'>
         <video
         src={shareVideo}
+        type="video/mp4"
+        Loop
+        controls={false}
+        muted
+        autoPlay
+        className='w-full h-full object-cover'
         />
-
+          <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+                <div className='p-5'>
+                  <img src={logo} width="130px" alt="logo"/>
+                </div>
+          </div>
       </div>
     </div>
   )
