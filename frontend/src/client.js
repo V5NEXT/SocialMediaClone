f4084b0ffedc6abc405caf24b4dad864d5ea8edb
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-11-26 22:41:26 +0100

    Sanity front end client file

---
+++ frontend/src/client.js
@@ -0,0 +1,15 @@
+import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+
+export const client = sanityClient({
+    projectId : '',
+    dataset: 'production',
+    apiVersion: '2022-11-26',
+    useCdn: true,
+    token: ''
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
