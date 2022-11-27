6667e725b5ca245fc5c74ed353783a032dd21fcb
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-11-27 08:05:11 +0100

    Home Component intial commit

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -8,8 +8,20 @@ import Pins from './Pins';
 import logo from '../assets/logo.png'
 
 const Home = () => {
+ const [toggleSidebar, setToggleSidebar] = useState(false);
+
   return (
-    <div>Home</div>
+    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
+    <div className='hidden md:flex h-screen flex-initial'>
+      <Sidebar/>
+      </div>
+      <div className='flex md:hidden flex-row'>
+        <HiMenu fontSize={40} className='cursor-pointer' onClick={()=> setToggleSidebar(false)}/>
+        <Link to="/">
+          <img src={logo} alt="logo" className='w-28'/>
+        </Link>
+    </div>
+    </div>
   )
 }
 
