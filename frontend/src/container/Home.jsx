a22de0869d9462fac1e38d7f005f05e0c81c7b4b
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-11-27 07:52:51 +0100

    Added imports and pins component

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -2,7 +2,10 @@ import React, {useState, useRef, useEffect} from 'react';
 import {HiMenu} from 'react-icons/hi';
 import {AiFillCloseCircle} from 'react-icons/ai';
 import { Link, Route, Routes} from 'react-router-dom';
-
+import {UserProfile, Sidebar} from '../components';
+import { client } from '../client';
+import Pins from './Pins';
+import logo from '../assets/logo.png'
 
 const Home = () => {
   return (
