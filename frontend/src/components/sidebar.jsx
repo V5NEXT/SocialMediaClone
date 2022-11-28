66a5d44b07d03f5c8102444de0985102f6b38c60
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-11-28 23:22:30 +0100

    Intermediate Commit

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -7,7 +7,14 @@ import logo from '../assets/logo.png';
 
 const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
 const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize'
-
+const categories = [
+    {name: 'Animals'},
+    {name: 'Coding'},
+    {name: 'Photos'},
+    {name: 'Bla'},
+    {name: 'li'},
+    {name: 'Blu'},
+]
 const sidebar = ({user, closeToggle}) => {
 
 
@@ -28,6 +35,12 @@ const sidebar = ({user, closeToggle}) => {
                         <RiHomeFill/>
                         Home
                     </NavLink>
+                    <h3 className='mt-2 px-5 text-base 2xl:text-xl'>Discover Categories</h3>
+                    {categories.slice(0, categories.length-1).map((category)=>(
+                        <NavLink to={`/category/${category.name}`}>
+
+                        </NavLink>
+                    ))}
                 </div>
             </div>
     </div>
