eb5accf0ff1ef813b817e7770b34c95af155d15a
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-12-01 20:14:55 +0100

    Navbar configured with temp

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -31,14 +31,15 @@ const sidebar = ({user, closeToggle}) => {
                   <img src={logo} alt="logo" className='w-full'/>
                 </Link>
                 <div className='flex flex-col gap-5'>
-                    <NavLink to="/" className={({isActive})=> isActive? isActiveStyle : isNotActiveStyle}>
+                    <NavLink to="/" className={({isActive})=> isActive? isActiveStyle : isNotActiveStyle} onClick={handleCLoseSidebar}>
                         <RiHomeFill/>
                         Home
                     </NavLink>
                     <h3 className='mt-2 px-5 text-base 2xl:text-xl'>Discover Categories</h3>
                     {categories.slice(0, categories.length-1).map((category)=>(
-                        <NavLink to={`/category/${category.name}`}>
-
+                        <NavLink to={`/category/${category.name}`}  className={({isActive})=> isActive? isActiveStyle : isNotActiveStyle} onClick={handleCLoseSidebar}
+                        key={category.name}>
+                                {category.name}
                         </NavLink>
                     ))}
                 </div>
