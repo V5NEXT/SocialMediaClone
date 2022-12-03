119e30db9d816e356c3eaf807fbd0ebb053fd00e
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-12-03 22:45:42 +0100

    Added Loader

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -7,6 +7,9 @@ import Spinner from './Spinner';
 
 
 const Feed = () => {
+  const [loading, setLoading] = useState(true)
+
+  if(loading) return <Spinner message="We are adding something"/>
   return (
     <div>Feed</div>
   )
--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Circles } from 'react-loader-spinner';
 
-const Spinner = () => {
+const Spinner = ({message}) => {
   return (
-    <div>Spinner</div>
+    <div className='flex felx-col justify-center items-center w-full h-full'>
+        <Circles
+        color="#00BFFF"
+        height={50}
+        width={200}
+        className='m-5'/>
+        <p className='text-lg text-center px-2'>{message}</p>
+    </div>
   )
 }
 
