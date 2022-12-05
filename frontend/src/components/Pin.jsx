d574f7bb8b8002e148d0cf8ca73c261631d67178
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-12-05 23:38:49 +0100

    Intermediate Commit

--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { urlFor } from '../client'
 
 const Pin = () => {
   return (
-    <div>Pin</div>
+    <div>
+        <img className='rounded-lg w-full' alt='user-post' src={urlFor(image).width(250).url}/>
+    </div>
   )
 }
 
