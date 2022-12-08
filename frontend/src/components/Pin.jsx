3a4cd75bb966468840cef9395d81481455b55df0
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-12-08 23:35:01 +0100

    Intermediate Commit

--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { urlFor } from '../client'
 
-const Pin = () => {
+const Pin = ({pin:{postedBy, image, _id, destination}}) => {
   return (
     <div>
-        <img className='rounded-lg w-full' alt='user-post' src={urlFor(image).width(250).url}/>
+        <img className='rounded-lg w-full' alt='user-post' src={urlFor(image.asset.url).width(250).url}/>
     </div>
   )
 }
