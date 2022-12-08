735a67157fc479facc5d69a6d00bcd51cb5f195f
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-12-08 23:50:10 +0100

    imports added

--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -1,5 +1,10 @@
-import React from 'react'
-import { urlFor } from '../client'
+import React, {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {v4 as uuidv4} from 'uuid';
+import { MdDownloadForOfline} from 'react-icons/md';
+import { AiTwotoneDelete} from 'react-icons/ai';
+import { BsFillArrorUpRightCircleFill} from 'react-icons/bs';
+import { urlFor, client } from '../client'
 
 const Pin = ({pin:{postedBy, image, _id, destination}}) => {
   console.log(image.asset.url)
