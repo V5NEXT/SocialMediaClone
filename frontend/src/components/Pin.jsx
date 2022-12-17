6694d64e069a09729c88bf3292eb2f111c7f0012
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-12-17 23:40:01 +0100

    Download icon issue fixed

--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -20,8 +20,7 @@ const Pin = ({pin:{postedBy, image, _id, destination}}) => {
        onClick={()=> Navigate(`/pin-detail/${_id}`)}
        className='relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out'
        >
-      </div>
-        <img className='rounded-lg w-full' alt='user-post' src={urlFor(image.asset.url).width(250).url()}/>
+           <img className='rounded-lg w-full' alt='user-post' src={urlFor(image.asset.url).width(250).url()}/>
         {postHovered && (
           <div className='absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pb-2 z-50' style={{height:'100%'}}>
             <div className='flex items-center justify-between'>
@@ -30,13 +29,15 @@ const Pin = ({pin:{postedBy, image, _id, destination}}) => {
                 href={`${image?.asset?.url}?dl=`}
                 download
                 onClick={(e)=> e.stopPropagation()}
-                className='bg-white w-9 h-9 rounded-full items-center justify-center text-dark text-xl opacity-75'>
+                className='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75'>
                     <MdDownloadForOffline/>
                 </a>
               </div>
             </div>
           </div>
         )}
+      </div>
+     
     </div>
   )
 }
