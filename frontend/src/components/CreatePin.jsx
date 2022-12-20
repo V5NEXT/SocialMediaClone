084d6bc4889f386bc0219a33af9a3f73c2a11f4d
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-12-20 15:54:53 +0100

    added all necessary hooks

--- frontend/src/components/CreatePin.jsx
+++ frontend/src/components/CreatePin.jsx
@@ -1,16 +1,29 @@
 import React, {useState} from 'react';
 import { AiOutlineCloudDownload } from 'react-icons/ai';
 import { MdDelete } from 'react-icons/md';
-import {userNavigate} from 'react-router-dom';
+import {useNavigate, userNavigate} from 'react-router-dom';
 
 import { client } from '../client';
+import { categories } from '../utils/data';
 import Spinner from './Spinner';
 
 //categories
 
 
-
 const CreatePin = ({user}) => {
+  const [title, settitle] = useState('');
+  const [about, setabout] = useState('');
+  const [destination, setdestination] = useState('');
+  const [loading, setloading] = useState(false);
+  const [fields, setfields] = useState(null);
+  const [category, setcategory] = useState(null);
+  const [imageAsset, setimageAsset] = useState(null);
+  const [WrongImageType, setWrongImageType] = useState(null);
+
+  const navigate = useNavigate();
+
+
+
   return (
     <div>CreatePin</div>
   )
