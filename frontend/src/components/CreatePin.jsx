782ce5af1037226071e4c6fda21dd9f50ed1da50
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-12-21 16:53:58 +0100

    Cloud Upload

--- frontend/src/components/CreatePin.jsx
+++ frontend/src/components/CreatePin.jsx
@@ -25,7 +25,32 @@ const CreatePin = ({user}) => {
 
 
   return (
-    <div>CreatePin</div>
+<div className='flex flex-col justify-center items-center mt-5 lg:h-4/5'>
+{fields&&(
+  <p className='text-red-500 mb-5 text-xl trasition-all duration-150 ease-in'>Please Fill in all the fields</p>
+)}
+<div className='flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5 w-full'>
+<div className='bg-secondaryColor p-3 flex flex-0.7 w-full'>
+ <div className='flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420'>
+  {loading && <Spinner/>}
+  {WrongImageType&&(
+    <p>Wrong Image Type</p>
+  )}
+  {!imageAsset?(
+    <label>
+      <div className='flex flex-col items-center justify-center h-full'>
+        <div className='flex flex-col justify-center items-center'>
+        <p className='font-bols text-2xl'>
+          <AiOutlineCloudDownload/>
+        </p>
+        </div>
+      </div>
+    </label>
+  ): (<p>SOmething</p>)}
+ </div>
+</div>
+</div>
+</div>
   )
 }
 
