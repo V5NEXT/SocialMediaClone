4c7f84140e64c06f090948c66f6d657ae5af0d98
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-12-23 23:53:34 +0100

    intermediate commit

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { MdDownloadForOffline } from 'react-icons/md';
+import {Link, useParams} from 'react-router-dom';
+import {v4 as uuidv4 } from 'uuid';
 
-const PinDetail = () => {
+const PinDetail = ({user}) => {
   return (
     <div>PinDetail</div>
   )
