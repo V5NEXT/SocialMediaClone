c75a94084dfb81d0457218119e55d24cedc93562
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-12-26 21:27:38 +0100

    Similar items

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -68,6 +68,7 @@ const PinDetail = ({user}) => {
   if(!pinDetail) return <Spinner message="Loadind Pin"/>
 
    return (
+    <>
     <div className='flex xl:flex-col flex-col m-auto bg-white' style={{maxWidth: '1500px', borderRadius:'32px'}}>
       <div className='flex justify-center items-center md:items-start flex-initial'>
         <img
@@ -142,6 +143,16 @@ const PinDetail = ({user}) => {
      </div>
       </div>
     </div>
+    {pins? (
+      <>
+      <h2 className='text-center font-bold text-2x mt-8 mb-4'>
+      More Like this
+      </h2>
+      </>
+    ):(
+      <Spinner message="Loading more pins"/>
+    )}
+    </>
   )
 }
 
