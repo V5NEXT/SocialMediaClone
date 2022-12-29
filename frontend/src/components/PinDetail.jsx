c5b0044ce2f35539f27214d37cb5fc92886040c7
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-12-29 13:44:47 +0100

    Fixed comments Thumbnail issue

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -17,12 +17,38 @@ const PinDetail = ({user}) => {
  const [addingComment, setAddingComment] = useState(false);
 
  const {pinId} = useParams();
+
+ const fetchPinDetails = ()=>{
+
+  let query = pinDetailQuery(pinId);
+
+  if(query){
+    client.fetch(query)
+    .then((data)=>{
+      setPinDetail(data[0]);
+
+      if(data[0]){
+        query = pinDetailMorePinQuery(data[0]);
+
+        client.fetch(query)
+        .then((res)=>{
+          setPins(res)
+        })
+      }
+    })
+  }
+
+}
+
+ useEffect(() => {
+  fetchPinDetails();
+}, [pinId])
+
  const addComment = () =>{
   if(comment){
     setAddingComment(true);
-
-    client.
-    patch(pinId)
+    client
+    .patch(pinId)
     .setIfMissing({comments:[]})
     .insert('after', 'comments[-1]',[{
       comment,
@@ -40,31 +66,8 @@ const PinDetail = ({user}) => {
     })
   }
 }
-  const fetchPinDetails = ()=>{
 
-    let query = pinDetailQuery(pinId);
 
-    if(query){
-      client.fetch(query)
-      .then((data)=>{
-        setPinDetail(data[0]);
-
-        if(data[0]){
-          query = pinDetailMorePinQuery(data[0]);
-
-          client.fetch(query)
-          .then((res)=>{
-            setPins(res)
-          })
-        }
-      })
-    }
-
-  }
-
-  useEffect(() => {
-    fetchPinDetails();
-  }, [pinId])
   if(!pinDetail) return <Spinner message="Loadind Pin"/>
 
    return (
